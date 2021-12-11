aedc1caaeaad72c58116223923e06b60a8cec4dd
matteoboccardo <91879241+matteoboccardo@users.noreply.github.com>
2021-12-11 20:44:16 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,9 +1,6 @@
-//
-// Created by squal on 11/12/2021.
-//
 #include "lib.h"
 #include <string.h>
-bool res (char list [10][20],char name [20],int &){
+bool res (char list [10][20],char name [20],int &i){
     for(int i=0;i<10;i++){
         if((strcmp(list[i],name))==0){
             return true;
