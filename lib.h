c807fb93ea2dcd9a12d8e5c8a2fd10db8eb3caa1
matteoboccardo <91879241+matteoboccardo@users.noreply.github.com>
2021-12-11 20:41:14 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,9 @@
+//
+// Created by bocca on 11/12/2021.
+//
 
+#ifndef INC_9_RICERCA_ARRAY2D_MATTEOBOCCARDO
+#define INC_9_RICERCA_ARRAY2D_MATTEOBOCCARDO
+//
+#endif //INC_9_RICERCA_ARRAY2D_MATTEOBOCCARDO_LIB_H
+bool res (char[][20],char[],int&);
