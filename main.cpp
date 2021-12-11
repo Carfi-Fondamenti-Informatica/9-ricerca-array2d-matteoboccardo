2e4c075972a213f2d5b338490638868f1975f5b1
matteoboccardo <91879241+matteoboccardo@users.noreply.github.com>
2021-12-11 20:40:13 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
-  return 0;
+    char list [10][20];
+    char name [20];
+    int a=0;
+    for(int i=0;i<10;i++){
+        cin>>list[i];
+    }
+    cin>>name;
+    if(res(list,name,a)){
+        cout<<a;
+    }else{
+        cout<<"non presente";
+    }
+    return 0;
 }
